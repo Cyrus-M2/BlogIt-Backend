a111e0a5506e0eecd4fedf7f85cef381dbf4c1ab
Cyrus-M2 <devsben@345@gmail.com>
2025-07-15 14:13:12 +0300

    adding secret key

--- src/app.ts
+++ src/app.ts
@@ -86,6 +86,7 @@ import userRoutes from "./routes/user";
 import uploadRoutes from "./routes/upload";
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -112,6 +113,10 @@ app.use("/api/upload", uploadRoutes);
 // Optional default route
 app.get("/", (_, res) => {
   res.send("BlogIt Backend Running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
